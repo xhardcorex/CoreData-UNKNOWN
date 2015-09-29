da40cd5877a33837e6ed734fedc7fd492a15d6d7
Your Name <nikshebeda@gmail.com>
2015-09-29 20:17:41 +0300

    Initial Commit

---
+++ CoreData UNKNOWN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData UNKNOWN
+//
+//  Created by Nik on 29.09.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
