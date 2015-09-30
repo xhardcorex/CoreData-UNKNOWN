9f07452974ae60ec6712c3e04465ca9d92f00710
Your Name <nikshebeda@gmail.com>
2015-09-30 20:39:59 +0300

    реадктор студентов

---
+++ NSCreateStudentTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSCreateStudentTableViewController.h
+//  CoreData UNKNOWN
+//
+//  Created by Nik on 30.09.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSCreateStudentTableViewController : UITableViewController
+
+@end
